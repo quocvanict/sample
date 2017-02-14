6fd470c9e429b3dedd5e4f28ea8b0d9e60ce0502
Do Quoc Van <quocvanict@gmail.com>
2017-02-14 11:09:05 +0700

    s

--- a.php
+++ a.php
@@ -1,6 +1,12 @@
 <?php echo "This is file a.php`";  ?>
 <?php
- echo "12345";
-echo "678:";
+<<<<<<< HEAD
+ echo "12347891011";
+echo "abc";
+echo "new line sample";
+=======
 
+ echo "12347891011";
+echo "abc";
+>>>>>>> 01a16849a465e7070fa7f75473d8f6894eb93694
 ?>
